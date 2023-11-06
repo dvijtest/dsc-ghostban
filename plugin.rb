b4a941574cd543f0333f857ce67848d44b6a4e2a
dvijtest <dvijtest@gmail.com>
2023-11-06 14:07:47 +0530

    changes in plugin files

--- plugin.rb
+++ plugin.rb
@@ -3,19 +3,19 @@
 # version: 0.0.3
 # authors: cap_dvij
 
-enabled_site_setting :shadowban_enabled
+enabled_site_setting :ghostban_enabled
 
 after_initialize do
 
-  module ::DiscourseShadowbanTopicView
+  module ::DiscourseGhostbanTopicView
     def filter_post_types(posts)
       result = super(posts)
-      if SiteSetting.shadowban_show_to_staff && @user&.staff?
+      if SiteSetting.ghostban_show_to_staff && @user&.staff?
         result
       else
         result.where(
           'posts.user_id NOT IN (SELECT u.id FROM users u WHERE username_lower IN (?) AND u.id != ?) AND NOT (posts.user_id IN (SELECT u.id FROM users u WHERE admin AND u.id != ?))',
-          SiteSetting.shadowban_users.split('|'),
+          SiteSetting.ghostban_users.split('|'),
           @user&.id || 0,
           @user&.id || 0
         )
@@ -24,18 +24,18 @@ after_initialize do
   end
 
   class ::TopicView
-    prepend ::DiscourseShadowbanTopicView
+    prepend ::DiscourseGhostbanTopicView
   end
 
-  module ::DiscourseShadowbanTopicQuery
+  module ::DiscourseGhostbanTopicQuery
     def default_results(options = {})
       result = super(options)
-      if SiteSetting.shadowban_show_to_staff && @user&.staff?
+      if SiteSetting.ghostban_show_to_staff && @user&.staff?
         result
       else
         result.where(
           'topics.user_id NOT IN (SELECT u.id FROM users u WHERE username_lower IN (?) AND u.id != ?) AND NOT (topics.user_id IN (SELECT u.id FROM users u WHERE admin AND u.id != ?))',
-          SiteSetting.shadowban_users.split('|'),
+          SiteSetting.ghostban_users.split('|'),
           @user&.id || 0,
           @user&.id || 0
         )
@@ -44,35 +44,35 @@ after_initialize do
   end
 
   class ::TopicQuery
-    prepend ::DiscourseShadowbanTopicQuery
+    prepend ::DiscourseGhostbanTopicQuery
   end
 
-  module ::DiscourseShadowbanPostAlerter
+  module ::DiscourseGhostbanPostAlerter
     def create_notification(user, type, post, opts = {})
-      if (SiteSetting.shadowban_show_to_staff && user&.staff?) || SiteSetting.shadowban_users.split('|').find_index(post.user&.username_lower).nil?
+      if (SiteSetting.ghostban_show_to_staff && user&.staff?) || SiteSetting.ghostban_users.split('|').find_index(post.user&.username_lower).nil?
         super(user, type, post, opts)
       end
     end
   end
 
   class ::PostAlerter
-    prepend ::DiscourseShadowbanPostAlerter
+    prepend ::DiscourseGhostbanPostAlerter
   end
 
-  module ::DiscourseShadowbanPostCreator
+  module ::DiscourseGhostbanPostCreator
     def update_topic_stats
-      if SiteSetting.shadowban_users.split('|').find_index(@post.user&.username_lower).nil?
+      if SiteSetting.ghostban_users.split('|').find_index(@post.user&.username_lower).nil?
         super
       end
     end
     def update_user_counts
-      if SiteSetting.shadowban_users.split('|').find_index(@post.user&.username_lower).nil?
+      if SiteSetting.ghostban_users.split('|').find_index(@post.user&.username_lower).nil?
         super
       end
     end
   end
 
   class ::PostCreator
-    prepend ::DiscourseShadowbanPostCreator
+    prepend ::DiscourseGhostbanPostCreator
   end
 end
