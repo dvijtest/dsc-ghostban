16f809146154533a00ddc8d45c6abe3619d30fc8
dvijtest <dvijtest@gmail.com>
2023-11-06 18:28:06 +0530

    changes in plugin rb 061123 0627

--- plugin.rb
+++ plugin.rb
@@ -1,11 +1,12 @@
 # name: ghostban
 # about: Hide a user's posts from everybody else
-# version: 0.0.4
+# version: 0.0.5
 # authors: cap_dvij
 
 enabled_site_setting :ghostban_enabled
 
 after_initialize do
+
   module ::DiscourseGhostbanTopicView
     def filter_post_types(posts)
       result = super(posts)
@@ -13,8 +14,10 @@ after_initialize do
         result
       else
         result.where(
-          'posts.user_id NOT IN (SELECT u.id FROM users u WHERE username_lower IN (?) AND u.id != ?) AND NOT (posts.user_id IN (SELECT u.id FROM users u WHERE admin AND u.id != ?))',
+          'posts.user_id NOT IN (SELECT u.id FROM users u WHERE username_lower IN (?) AND u.id != ?) AND NOT (posts.user_id IN (SELECT u.id FROM users u WHERE admin AND u.id != ?)) OR (posts.user_id = ? OR posts.user_id IN (SELECT u.id FROM users u WHERE admin AND u.id != ?))',
           SiteSetting.ghostban_users.split('|'),
+          @user&.id || 0,
+          @user&.id || 0,
           @user&.id || 0,
           @user&.id || 0
         )
@@ -48,7 +51,7 @@ after_initialize do
 
   module ::DiscourseGhostbanPostAlerter
     def create_notification(user, type, post, opts = {})
-      if (SiteSetting.ghostban_show_to_staff && user&.staff?) || SiteSetting.ghostban_users.split('|').include?(post.user&.username_lower)
+      if (SiteSetting.ghostban_show_to_staff && user&.staff?) || SiteSetting.ghostban_users.split('|').find_index(post.user&.username_lower).nil? || user&.admin?
         super(user, type, post, opts)
       end
     end
@@ -60,13 +63,13 @@ after_initialize do
 
   module ::DiscourseGhostbanPostCreator
     def update_topic_stats
-      if SiteSetting.ghostban_users.split('|').include?(@post.user&.username_lower)
+      if SiteSetting.ghostban_users.split('|').find_index(@post.user&.username_lower).nil? || @post.user&.admin?
         super
       end
     end
 
     def update_user_counts
-      if SiteSetting.ghostban_users.split('|').include?(@post.user&.username_lower)
+      if SiteSetting.ghostban_users.split('|').find_index(@post.user&.username_lower).nil? || @post.user&.admin?
         super
       end
     end
