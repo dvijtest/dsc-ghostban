116e063f9d552e3ea5928d0a4f1a8a985b7943ee
dvijtest <dvijtest@gmail.com>
2023-11-07 12:20:11 +0530

    restored 0.0.3 updated to version 0.0.7

--- plugin.rb
+++ plugin.rb
@@ -1,20 +1,24 @@
 # name: ghostban
 # about: Hide a user's posts from everybody else
-# version: 0.0.6
+# version: 0.0.7
 # authors: cap_dvij
 
 enabled_site_setting :ghostban_enabled
 
 after_initialize do
+
   module ::DiscourseGhostbanTopicView
     def filter_post_types(posts)
       result = super(posts)
       if SiteSetting.ghostban_show_to_staff && @user&.staff?
         result
       else
-        ghostbanned_user_ids = User.where(username_lower: SiteSetting.ghostban_users.split('|')).pluck(:id)
-        ghostbanned_user_ids << @user&.id if @user&.admin?
-        result.where('posts.user_id IN (?) OR topics.archetype = ?', ghostbanned_user_ids, 'private_message')
+        result.where(
+          'posts.user_id NOT IN (SELECT u.id FROM users u WHERE username_lower IN (?) AND u.id != ?) AND NOT (posts.user_id IN (SELECT u.id FROM users u WHERE admin AND u.id != ?))',
+          SiteSetting.ghostban_users.split('|'),
+          @user&.id || 0,
+          @user&.id || 0
+        )
       end
     end
   end
@@ -29,9 +33,12 @@ after_initialize do
       if SiteSetting.ghostban_show_to_staff && @user&.staff?
         result
       else
-        ghostbanned_user_ids = User.where(username_lower: SiteSetting.ghostban_users.split('|')).pluck(:id)
-        ghostbanned_user_ids << @user&.id if @user&.admin?
-        result.where('topics.user_id IN (?) OR topics.archetype = ?', ghostbanned_user_ids, 'private_message')
+        result.where(
+          'topics.user_id NOT IN (SELECT u.id FROM users u WHERE username_lower IN (?) AND u.id != ?) AND NOT (topics.user_id IN (SELECT u.id FROM users u WHERE admin AND u.id != ?))',
+          SiteSetting.ghostban_users.split('|'),
+          @user&.id || 0,
+          @user&.id || 0
+        )
       end
     end
   end
@@ -42,7 +49,7 @@ after_initialize do
 
   module ::DiscourseGhostbanPostAlerter
     def create_notification(user, type, post, opts = {})
-      if (SiteSetting.ghostban_show_to_staff && user&.staff?) || SiteSetting.ghostban_users.split('|').include?(post.user&.username_lower)
+      if (SiteSetting.ghostban_show_to_staff && user&.staff?) || SiteSetting.ghostban_users.split('|').find_index(post.user&.username_lower).nil?
         super(user, type, post, opts)
       end
     end
@@ -54,13 +61,12 @@ after_initialize do
 
   module ::DiscourseGhostbanPostCreator
     def update_topic_stats
-      if SiteSetting.ghostban_users.split('|').include?(@post.user&.username_lower)
+      if SiteSetting.ghostban_users.split('|').find_index(@post.user&.username_lower).nil?
         super
       end
     end
-
     def update_user_counts
-      if SiteSetting.ghostban_users.split('|').include?(@post.user&.username_lower)
+      if SiteSetting.ghostban_users.split('|').find_index(@post.user&.username_lower).nil?
         super
       end
     end
