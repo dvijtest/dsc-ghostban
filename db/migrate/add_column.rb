97f878687b309ba72f32f16e8d573cb120015d2d
dvijtest <dvijtest@gmail.com>
2023-11-09 15:34:20 +0530

    plugin 0.0.12

--- db/migrate/add_column.rb
+++ db/migrate/add_column.rb
@@ -1,7 +1,6 @@
 # Create a migration to add the is_reply_to_ghostbanned column to the posts table
 class AddIsReplyToGhostbannedToPosts < ActiveRecord::Migration[6.0]
-    def change
-      add_column :posts, :is_reply_to_ghostbanned, :boolean, default: false
-    end
+  def change
+    add_column :posts, :is_reply_to_ghostbanned, :boolean, default: false
   end
-  
+end
--- plugin.rb
+++ plugin.rb
@@ -6,31 +6,11 @@
 enabled_site_setting :ghostban_enabled
 
 after_initialize do
-  # Step 1: Add a boolean column is_reply_to_ghostbanned to the posts table
-  add_column :posts, :is_reply_to_ghostbanned, :boolean, default: false, null: false
+  #   if !PostCustomField.new.respond_to?(:is_reply_to_ghostbanned)
+  #     require Rails.root.join('plugins', 'dsc-ghostban', 'migrations', 'add_column')
+  #     AddColumns.new.up # <-- this runs the migration
+  #   end
 
-  # Step 2: Add logic to update is_reply_to_ghostbanned column
-  on(:post_created) do |post, _params|
-    update_ghostban_status(post)
-  end
-
-  on(:post_edited) do |post, _params|
-    update_ghostban_status(post)
-  end
-
-  def update_ghostban_status(post)
-    post.is_reply_to_ghostbanned = should_mark_as_ghostbanned?(post)
-    post.save!
-  end
-
-  def should_mark_as_ghostbanned?(post)
-    return false unless post.reply_to_user_id
-
-    ghostbanned_usernames = SiteSetting.ghostban_users.split('|')
-    ghostbanned_usernames.include?(User.find(post.reply_to_user_id)&.username_lower)
-  end
-
-  # Step 3: Modify the filter_post_types method
   module ::DiscourseGhostbanTopicView
     def filter_post_types(posts)
       result = super(posts)
@@ -38,10 +18,16 @@ after_initialize do
         result
       else
         result.where(
-          'posts.user_id NOT IN (SELECT u.id FROM users u WHERE username_lower IN (?) AND u.id != ?) AND NOT (posts.is_reply_to_ghostbanned AND NOT posts.user_id IN (SELECT u.id FROM users u WHERE admin))',
+          'posts.user_id NOT IN (SELECT u.id FROM users u WHERE username_lower IN (?) AND u.id != ?) AND NOT (posts.user_id IN (SELECT u.id FROM users u WHERE admin AND u.id != ?))',
           SiteSetting.ghostban_users.split('|'),
+          @user&.id || 0,
           @user&.id || 0
         )
+        #         result.where(
+        #           'posts.user_id NOT IN (SELECT u.id FROM users u WHERE username_lower IN (?) AND u.id != ?) AND NOT (posts.is_reply_to_ghostbanned AND NOT posts.user_id IN (SELECT u.id FROM users u WHERE admin))',
+        #           SiteSetting.ghostban_users.split('|'),
+        #           @user&.id || 0
+        #           )
       end
     end
   end
