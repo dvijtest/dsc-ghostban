6ecf685ba80bb8dda7bdd7c2ebd1d90fd52adeca
dvijtest <dvijtest@gmail.com>
2023-11-09 15:35:13 +0530

    plugin 0.0.12

--- db/migrate/add_column.rb
+++ db/migrate/add_column.rb
@@ -1,4 +1,4 @@
-# Create a migration to add the is_reply_to_ghostbanned column to the posts table
+
 class AddIsReplyToGhostbannedToPosts < ActiveRecord::Migration[6.0]
   def change
     add_column :posts, :is_reply_to_ghostbanned, :boolean, default: false
