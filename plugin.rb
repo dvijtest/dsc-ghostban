adaa5ff329c13ffb04e83c53bff19a00e10288af
dvijtest <dvijtest@gmail.com>
2023-11-17 16:15:05 +0530

    plugin 0.0.33

--- plugin.rb
+++ plugin.rb
@@ -1,80 +1,71 @@
 # name: dsc-ghostban
 # about: Hide a user's posts from everybody else
-# version: 0.0.32
+# version: 0.0.33
 # authors: cap_dvij
 
-enabled_site_setting :ghostban_enabled
+enabled_site_setting :ghostmode_enabled
 
 after_initialize do
-  module ::DiscourseGhostbanTopicView
+
+  module ::DiscourseShadowbanTopicView
     def filter_post_types(posts)
       result = super(posts)
-
-      if SiteSetting.ghostban_show_to_staff && @user&.staff?
+      if SiteSetting.ghostmode_show_to_staff && @user&.staff?
         result
       else
         result.where(
-          'posts.user_id NOT IN (SELECT u.id FROM users u WHERE username_lower IN (?) AND u.id != ?) AND NOT (posts.user_id IN (SELECT u.id FROM users u WHERE admin AND u.id != ?)) OR (posts.user_id = ?)',
-          SiteSetting.ghostban_users.split('|'),
-          @user&.id || 0,
-          @user&.id || 0,
-          @user&.id || 0,
+          'posts.id NOT IN (?) AND u.id != ?',
+          SiteSetting.ghostmode_posts.split('|'),
+          @user&.id || 0
         )
       end
     end
   end
 
   class ::TopicView
-    prepend ::DiscourseGhostbanTopicView
+    prepend ::DiscourseShadowbanTopicView
   end
 
-  module ::DiscourseGhostbanTopicQuery
+  module ::DiscourseShadowbanTopicQuery
     def default_results(options = {})
       result = super(options)
-      if SiteSetting.ghostban_show_to_staff && @user&.staff?
+      if SiteSetting.ghostmode_show_to_staff && @user&.staff?
         result
       else
         result.where(
-          'topics.user_id NOT IN (SELECT u.id FROM users u WHERE username_lower IN (?) AND u.id != ?) OR (topics.user_id = ?)',
-          SiteSetting.ghostban_users.split('|'),
-          @user&.id || 0,
-          @user&.id || 0,
+          'topics.id NOT IN (?) AND u.id != ?',
+          SiteSetting.ghostmode_topics.split('|'),
+          @user&.id || 0
         )
       end
     end
   end
 
   class ::TopicQuery
-    prepend ::DiscourseGhostbanTopicQuery
+    prepend ::DiscourseShadowbanTopicQuery
   end
 
-  module ::DiscourseGhostbanPostAlerter
+  module ::DiscourseShadowbanPostAlerter
     def create_notification(user, type, post, opts = {})
-      if (SiteSetting.ghostban_show_to_staff && user&.staff?) || (user&.admin? && post.topic.user_id != user.id) || SiteSetting.ghostban_users.split('|').find_index(post.user&.username_lower).nil?
+      if (SiteSetting.ghostmode_show_to_staff && user&.staff?) || SiteSetting.ghostmode_posts.split('|').find_index(post.id).nil?
         super(user, type, post, opts)
       end
     end
   end
 
   class ::PostAlerter
-    prepend ::DiscourseGhostbanPostAlerter
+    prepend ::DiscourseShadowbanPostAlerter
   end
 
-  module ::DiscourseGhostbanPostCreator
+  module ::DiscourseShadowbanPostCreator
     def update_topic_stats
-      return unless SiteSetting.ghostban_users.split('|').find_index(@post.user&.username_lower).nil?
-
-      super
-    end
-
-    def update_user_counts
-      return unless SiteSetting.ghostban_users.split('|').find_index(@post.user&.username_lower).nil?
-
-      super
+      if SiteSetting.ghostmode_topics.split('|').find_index(@post.id).nil?
+        super
+      end
     end
   end
 
   class ::PostCreator
-    prepend ::DiscourseGhostbanPostCreator
+    prepend ::DiscourseShadowbanPostCreator
   end
 end
