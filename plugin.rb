58c69cde97b0bb8d05bfe019aa2273367ae0a585
dvijtest <dvijtest@gmail.com>
2023-11-17 14:03:59 +0530

    plugin 0.0.31

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: dsc-ghostban
 # about: Hide a user's posts from everybody else
-# version: 0.0.30
+# version: 0.0.31
 # authors: cap_dvij
 
 enabled_site_setting :ghostban_enabled
@@ -14,19 +14,13 @@ after_initialize do
         result
       else
         result.where(
-          'posts.user_id NOT IN (SELECT u.id FROM users u WHERE username_lower IN (?) AND u.id != ?) AND NOT (posts.user_id IN (SELECT u.id FROM users u WHERE admin AND u.id != ?))',
+          'posts.user_id NOT IN (SELECT u.id FROM users u WHERE username_lower IN (?) AND u.id != ?) AND NOT (posts.user_id IN (SELECT u.id FROM users u WHERE admin AND u.id != ?)) OR (posts.user_id = ? AND posts.raw LIKE ?)',
           SiteSetting.ghostban_users.split('|'),
           @user&.id || 0,
-          @user&.id || 0
+          @user&.id || 0,
+          @user&.id || 0,
+          "%#{I18n.t('subject.reply_to', username: @user&.username)}%"
         )
-      end
-    end
-
-    def filter_admin_posts(posts)
-      if SiteSetting.ghostban_show_to_staff && @user&.staff?
-        posts
-      else
-        posts.where("posts.user_id IN (SELECT u.id FROM users u WHERE admin AND u.id != ?)", @user&.id || 0)
       end
     end
   end
@@ -42,11 +36,13 @@ after_initialize do
         result
       else
         result.where(
-          'topics.user_id NOT IN (SELECT u.id FROM users u WHERE username_lower IN (?) AND u.id != ?)',
-          SiteSetting.ghostban_users.split('|'),
-          @user&.id || 0
-        )
-      end
+	'topics.user_id NOT IN (SELECT u.id FROM users u WHERE username_lower IN (?) AND u.id != ?) OR (topics.user_id = ? AND topics.title LIKE ?)',
+	SiteSetting.ghostban_users.split('|'),          
+	@user&.id || 0,
+	@user&.id || 0,          
+	"%#{I18n.t('subject.reply_to', username: @user&.username)}%"        
+	)      
+	end
     end
   end
 
@@ -56,7 +52,7 @@ after_initialize do
 
   module ::DiscourseGhostbanPostAlerter
     def create_notification(user, type, post, opts = {})
-      if (SiteSetting.ghostban_show_to_staff && user&.staff?) || SiteSetting.ghostban_users.split('|').find_index(post.user&.username_lower).nil?
+      if (SiteSetting.ghostban_show_to_staff && user&.staff?) || (user&.admin? && post.topic.user_id != user.id) || SiteSetting.ghostban_users.split('|').find_index(post.user&.username_lower).nil?
         super(user, type, post, opts)
       end
     end
@@ -77,14 +73,6 @@ after_initialize do
       return unless SiteSetting.ghostban_users.split('|').find_index(@post.user&.username_lower).nil?
 
       super
-    end
-
-    def admin_reply_to_other_users
-      @post.topic.users.each do |user|
-        next if user.id == @post.user.id || user.staff?
-
-        PostAlerter.bump(@post, user)
-      end
     end
   end
 
